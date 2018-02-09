9ec6a5a07bf0e82ed09ab356e7f75accc154e49c
ashraymp <mpashray@gmail.com>
2018-02-10 00:05:33 +0530

    Add files via upload
    Connected IR senors which detects obstacles and controls wheel movements.

---
+++ motor_temp.ino
@@ -0,0 +1,67 @@
+/*------ Arduino Line Follower Code----- */
+/*-------definning Inputs------*/
+#define LS 16      // left sensor
+#define RS 18      // right sensor
+
+/*-------definning Outputs------*/
+#define LM1  13      // left motor
+#define LM2 5       // left motor
+#define RM1 6       // right motor
+#define RM2 7       // right motor
+
+void setup()
+{
+  pinMode(LS, INPUT);
+  pinMode(RS, INPUT);
+  pinMode(LM1, OUTPUT);
+  pinMode(LM2, OUTPUT);
+  pinMode(RM1, OUTPUT);
+  pinMode(RM2, OUTPUT);
+}
+
+void loop()
+{
+  //digitalWrite(LM1, HIGH);
+   /*digitalWrite(LM1, 0);
+    digitalWrite(LM2, LOW);
+    digitalWrite(RM1, 0);
+    digitalWrite(RM2, LOW);
+  */
+  if(digitalRead(LS) && digitalRead(RS))     // Move Forward
+  {
+    digitalWrite(LM1, HIGH);
+    digitalWrite(LM2, LOW);
+    digitalWrite(RM1, HIGH);
+    digitalWrite(RM2, LOW);
+    Serial.println("Forward");
+  }
+  
+  if(!(digitalRead(LS)) && digitalRead(RS))     // Turn right
+  {
+    digitalWrite(LM1, LOW);
+    digitalWrite(LM2, LOW);
+    digitalWrite(RM1, HIGH);
+    digitalWrite(RM2, LOW);
+    Serial.println("turn right");
+  }
+  
+  if(digitalRead(LS) && !(digitalRead(RS)))     // turn left
+  {
+    digitalWrite(LM1, HIGH);
+    digitalWrite(LM2, LOW);
+    digitalWrite(RM1, LOW);
+    digitalWrite(RM2, LOW);
+    Serial.println("left ");
+  }
+  
+  if(!(digitalRead(LS)) && !(digitalRead(RS)))     // stop
+  {
+    digitalWrite(LM1, LOW);
+    digitalWrite(LM2, LOW);
+    digitalWrite(RM1, LOW);
+    digitalWrite(RM2, LOW);
+    Serial.println("stop");
+  }
+  //stop();
+}
+
